1d9234fccfec6275d85830c70ed9b270e1e9bc40
Drake Main <drakemain.dev@gmail.com>
2022-03-13 16:10:16 -0700

    serve static files

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,10 @@ const wordManager = new Word();
 const PORT = 8080;
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', express.static('client'));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-});
-
-app.get('/', (_, res) => {
-    res.send('Hello World');
 });
 
 app.get('/guess/:word', async (req, res) => {
